9b0d85de39791171f4784db70d6ecd31fbb83e3d
Ryan Le <leryan027@gmail.com>
2026-06-11 15:59:18 -0700

    Add the very first obstacle in the game

--- Game.java
+++ Game.java
@@ -13,6 +13,20 @@ public class Game {
     }
 
     public void start() {
-        
+        System.out.println("\nWelcome player.\nYou have just been teleported to a dungeon in an alternate dimension.\nI am the spirit who will guide you throughout this journey.\nYour objective is to earn 6767 gold and not die to escape this nightmare.\nTo begin, create a name for yourself.\nGood luck, and may you make it out alive.\n");
+
+        System.out.print("Enter your name: ");
+        String name = input.nextLine();
+        System.out.println();
+        player = new Player(name);
+
+        System.out.println("You step inside the dungeon and encounter an armored bear. What do you do?");
+        System.out.println("1. Pick up a sword from the corpse beside you and throw it like a spear at the bear's head.");
+        System.out.println("2. Run because you are scared.");
+        System.out.println("3. Charge the bear and attempt to wrestle it.\n");
+
+        System.out.print("Enter your choice number: ");
+        int choice = input.nextInt();
+        System.out.println();
     }
 }
